7f369f2d51c659f41ccd3cdb1ff3cc6b36f1b2c7
Murli913 <murlitalreja913@gmail.com>
2024-01-10 15:43:38 +0530

    final commit

---
+++ studentsystem/src/test/java/com/murlicode/studentsystem/StudentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murlicode.studentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
